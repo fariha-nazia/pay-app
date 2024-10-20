2966a10b754300c3157d87fd84eadc088efff34a
Fariha Nazia <farihanazia@Farihas-MBP.fritz.box>
2024-10-20 12:02:03 +0200

    the form is working

--- js/login.js
+++ js/login.js
@@ -1,11 +1,50 @@
+// Better way of doing the work
+
+// step 1: make the button work
+document.getElementById('button-login').addEventListener('click', function(event){ //we need to put event as a parameter inside because we are working in side a form amd when we inside a form we need to do this
+        // step 1.1: we need to write event.preventDefalut(), because a from has a nature of reloading the page by itself and this is a problem. to stop this we need to write this
+        // console.log('button clicked')
+        event.preventDefault();
+
+        // step 2: take input from pass and number field when you click on the button
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(phoneNumber, pinNumber);
+
+        // step 3: now we need to validate it. This process of validation is not correct, in future we will do this using server and other things.
+        if(phoneNumber === '123' && pinNumber === '3333'){
+                console.log('Logged in');
+        }
+        else{
+                alert('invalid input');
+        }
+});
+
+
+
+
+
 // console.log('connected');
 
 // step 1: set event handler
-document.getElementById('button-login').addEventListener('click', function(event){
-        event.preventDefault(); // stops reloading the page / prevent default behaviour
-        console.log('working');
+// document.getElementById('button-login').addEventListener('click', function(event){
+//         event.preventDefault(); // stops reloading the page / prevent default behaviour
+//         // console.log('working');
 
-        // step 2: get the phone number
-        const phoneNumber = document.getElementById('phone-number').value;
-        console.log(phoneNumber);
-})
+//         // step 2: get the phone number
+//         const phoneNumber = document.getElementById('phone-number').value;
+//         console.log(phoneNumber);
+
+//         const pinNumber = document.getElementById('pin-number').value;
+//         console.log(pinNumber);
+
+//         /* 
+//         A temporary way to check if you are giving the correct phone number and password is given below. LAter on we will work with server to do this.
+//         */
+//        if(phoneNumber === 12345 && pinNumber === 1234){
+//         console.log('You logged in');
+//        }
+//        else{
+//         alert('Wrong input');
+//        }
+// })
