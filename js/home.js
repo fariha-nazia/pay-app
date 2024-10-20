73508135aa56fe5355d2c3b010e99f51796935bd
Fariha Nazia <farihanazia@Farihas-MBP.fritz.box>
2024-10-20 16:10:40 +0200

    add money working

--- js/home.js
+++ js/home.js
@@ -14,5 +14,23 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
 
+    // step 3: verify the pin number ---> this is a wrong way to do validation
+    if(pinNumberInput === '3333'){
+        // step 4: get the current balance ---> to get the value we can see that it is in the div not in input so we need to use innerText
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
 
+        // step 5: we need to add adMoneyInput with balance to find the total
+        // const newBalance = balance + addMoneyInput; ---> not the correct way because we are adding string not numbers 
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step 6: update the new balance in the DOM/UI/Web
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Your Pin is Wrong');
+    }
 });
